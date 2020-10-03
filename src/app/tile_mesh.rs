92b7687f27fe3acc675d474a588b8a8a08205e16
kuviman <kuviman@gmail.com>
2020-10-03 17:43:16 +0400

    Better water rendering

--- src/app/tile_mesh.rs
+++ src/app/tile_mesh.rs
@@ -10,52 +10,78 @@ impl TileMesh {
         let mut mesh = Vec::new();
         let tiles: HashMap<Vec2<usize>, model::Tile> =
             tiles.iter().map(|tile| (tile.pos, tile.clone())).collect();
+        let mut append_quad =
+            |p: Vec2<usize>, h00: f32, h10: f32, h11: f32, h01: f32, a_color: Color<f32>| {
+                let p = p.map(|x| x as f32);
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x, p.y, h00),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x + 1.0, p.y, h10),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x + 1.0, p.y + 1.0, h11),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x, p.y, h00),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x + 1.0, p.y + 1.0, h11),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+                mesh.push(ez3d::Vertex {
+                    a_pos: vec3(p.x, p.y + 1.0, h01),
+                    a_normal: vec3(0.0, 0.0, 0.0),
+                    a_color,
+                });
+            };
         let mut append = |tile00: &model::Tile| -> Option<()> {
             let tile01 = tiles.get(&(tile00.pos + vec2(0, 1)))?;
             let tile11 = tiles.get(&(tile00.pos + vec2(1, 1)))?;
             let tile10 = tiles.get(&(tile00.pos + vec2(1, 0)))?;
-            let a_color = match tile00.ground_type {
-                model::GroundType::Water => Color::BLUE,
-                model::GroundType::Sand => Color::YELLOW,
-            };
             let p00 = tile00.pos.map(|x| x as f32).extend(tile00.height);
             let p01 = tile01.pos.map(|x| x as f32).extend(tile01.height);
             let p11 = tile11.pos.map(|x| x as f32).extend(tile11.height);
             let p10 = tile10.pos.map(|x| x as f32).extend(tile10.height);
-            mesh.push(ez3d::Vertex {
-                a_pos: p00,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
-            mesh.push(ez3d::Vertex {
-                a_pos: p10,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
-            mesh.push(ez3d::Vertex {
-                a_pos: p11,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
-            mesh.push(ez3d::Vertex {
-                a_pos: p00,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
-            mesh.push(ez3d::Vertex {
-                a_pos: p11,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
-            mesh.push(ez3d::Vertex {
-                a_pos: p01,
-                a_normal: vec3(0.0, 0.0, 0.0),
-                a_color,
-            });
+            append_quad(
+                tile00.pos,
+                tile00.height,
+                tile10.height,
+                tile11.height,
+                tile01.height,
+                match tile00.ground_type {
+                    model::GroundType::Water => Color::rgb(0.8, 0.8, 0.0),
+                    model::GroundType::Sand => Color::YELLOW,
+                },
+            );
             Some(())
         };
-        for tile00 in tiles.values() {
-            append(tile00);
+        for tile in tiles.values() {
+            append(tile);
+        }
+        for tile in tiles.values() {
+            if tiles.get(&(tile.pos + vec2(0, 1))).is_some()
+                && tiles.get(&(tile.pos + vec2(1, 1))).is_some()
+                && tiles.get(&(tile.pos + vec2(1, 0))).is_some()
+            {
+                append_quad(
+                    tile.pos,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    Color::rgba(0.0, 0.5, 1.0, 0.5),
+                );
+            }
         }
         ez3d::calc_normals(&mut mesh);
         Self {
