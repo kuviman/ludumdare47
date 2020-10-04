3a4084ce1f3c92c86ab5f188695524e96daf4bcf
kuviman <kuviman@gmail.com>
2020-10-04 13:06:54 +0400

    Darker night

--- src/app/ez3d/program.glsl
+++ src/app/ez3d/program.glsl
@@ -16,8 +16,8 @@ uniform mat4 u_view_matrix;
 
 uniform vec3 u_light_direction;
 
-#define AMBIENT 0.3
-#define AMBIENT2 0.3
+#define AMBIENT vec3(0.1, 0.1, 0.1)
+#define AMBIENT2 vec3(0.1, 0.1, 0.4)
 
 void main()
 {
@@ -25,9 +25,9 @@ void main()
     pos.xy = rotate(pos.xy, i_rotation);
     vec3 normal = a_normal;
     normal.xy = rotate(normal.xy, i_rotation);
-    float light = AMBIENT + max(0.0, dot(a_normal, vec3(0.0, 0.0, 1.0))) * AMBIENT2 + max(0.0, dot(a_normal, u_light_direction)) * (1.0 - AMBIENT - AMBIENT2);
+    vec3 light = AMBIENT + max(0.0, dot(a_normal, vec3(0.0, 0.0, 1.0))) * AMBIENT2 + max(0.0, dot(a_normal, u_light_direction)) * (vec3(1.0) - AMBIENT - AMBIENT2);
     vec4 color = a_color * i_color;
-    v_color = vec4(color.xyz * min(light + a_emission, 1.0), color.w);
+    v_color = vec4(color.xyz * min(light + vec3(a_emission), vec3(1.0)), color.w);
     gl_Position = u_projection_matrix * u_view_matrix * vec4(i_pos + pos * i_size, 1.0);
 }
 #endif
