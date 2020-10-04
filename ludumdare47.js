48eee28ac55a07343d1ce36c5b42d2ddd32d2ca6
kuviman <kuviman@users.noreply.github.com>
2020-10-04 07:40:01 +0000

    deploy: 3819a2bff53ef04363d6561f0dfa51cdb828fbd8

--- ludumdare47.js
+++ ludumdare47.js
@@ -825,24 +825,24 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 43, __wbg_adapter_29);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper851 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 304, __wbg_adapter_32);
+    imports.wbg.__wbindgen_closure_wrapper852 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 306, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper853 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 304, __wbg_adapter_35);
+    imports.wbg.__wbindgen_closure_wrapper854 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 306, __wbg_adapter_35);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper855 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 304, __wbg_adapter_38);
+    imports.wbg.__wbindgen_closure_wrapper856 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 306, __wbg_adapter_38);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper857 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 304, __wbg_adapter_41);
+    imports.wbg.__wbindgen_closure_wrapper858 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 306, __wbg_adapter_41);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper859 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 304, __wbg_adapter_44);
+    imports.wbg.__wbindgen_closure_wrapper860 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 306, __wbg_adapter_44);
         return addHeapObject(ret);
     };
     imports['./snippets/geng-core-07e0ee00576cfe86/src/window/web.js'] = __wbg_star0;
