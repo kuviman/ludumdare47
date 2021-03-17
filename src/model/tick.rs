2c7b3519e3a5ba27db6ff46a8548394d0d621986
Alexander <sasha.kudasov04@gmail.com>
2021-03-17 20:46:59 +0500

    Refactor Model::tick()

--- src/model/tick.rs
+++ src/model/tick.rs
@@ -6,134 +6,96 @@ impl Model {
         for id in ids {
             if let Some(entity) = self.chunked_world.get_entity(id) {
                 let mut entity = entity.clone();
-
-                if entity.components.player.is_some() {
-                    self.player_action(&mut entity);
-                }
-
-                // Collide with entities
-                if let Some(entity_collidable) = entity.components.collidable.clone() {
-                    for other in self
-                        .chunked_world
-                        .entities_mut()
-                        .filter(|other| other.id != id && other.components.collidable.is_some())
-                    {
-                        let dir = entity.pos.unwrap() - other.pos.unwrap();
-                        let distance = dir.len();
-                        if distance <= entity.size.unwrap() + other.size.unwrap() {
-                            let penetration = entity.size.unwrap() + other.size.unwrap() - distance;
-                            let normal = dir / distance;
-
-                            match entity_collidable.collision_type {
-                                CollisionType::Static => {
-                                    *other.pos.as_mut().unwrap() += -normal * penetration;
-                                }
-                                CollisionType::Pushable => {
-                                    match other
-                                        .components
-                                        .collidable
-                                        .as_ref()
-                                        .unwrap()
-                                        .collision_type
-                                    {
-                                        CollisionType::Static => {
-                                            *entity.pos.as_mut().unwrap() += normal * penetration;
-                                        }
-                                        CollisionType::Pushable => {
-                                            *entity.pos.as_mut().unwrap() +=
-                                                normal * penetration / 2.0;
-                                            *other.pos.as_mut().unwrap() +=
-                                                normal * penetration / 2.0;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if let Some(size) = entity.size {
-                    // Collide with tiles
-                    for x in (-size.ceil() as i64)..(size.ceil() as i64 + 1) {
-                        for y in (-size.ceil() as i64)..(size.ceil() as i64 + 1) {
-                            let tile_pos =
-                                get_tile_pos(vec2(x as f32, y as f32) + entity.pos.unwrap());
-                            if let Some((normal, penetration)) = match self
-                                .chunked_world
-                                .get_tile(tile_pos)
-                            {
-                                Some(tile) => {
-                                    if self.resource_pack.biome_properties[&tile.biome].collidable {
-                                        Self::collide_tile(entity.pos.unwrap(), size, tile_pos, 1.0)
-                                    } else {
-                                        None
-                                    }
-                                }
-                                None => None,
-                            } {
-                                *entity.pos.as_mut().unwrap() += normal * penetration;
-                            }
-                        }
-                    }
-                }
-
-                *self.chunked_world.get_entity_mut(id).unwrap() = entity;
-                self.chunked_world.update_entity(id);
+                self.update_entity(entity);
             }
         }
     }
 
-    fn collide_tile(
-        circle_pos: Vec2<f32>,
-        circle_radius: f32,
-        tile_pos: Vec2<i64>,
-        tile_size: f32,
-    ) -> Option<(Vec2<f32>, f32)> {
-        let up = circle_pos.y - tile_pos.y as f32 - tile_size;
-        let down = tile_pos.y as f32 - circle_pos.y;
-        let right = circle_pos.x - tile_pos.x as f32 - tile_size;
-        let left = tile_pos.x as f32 - circle_pos.x;
+    fn update_entity(&mut self, mut entity: Entity) {
+        let entity_id = entity.id;
+        if entity.components.player.is_some() {
+            self.player_action(&mut entity);
+        }
 
-        let (dy, ny) = if up.abs() < down.abs() {
-            (up, 1.0)
-        } else {
-            (down, -1.0)
-        };
-        let (dx, nx) = if right.abs() < left.abs() {
-            (right, 1.0)
-        } else {
-            (left, -1.0)
-        };
-
-        // Find direction and distance from the tile to the center point
-        let (normal, distance) = if dx <= 0.0 && dy <= 0.0 {
-            // Inside
-            if dx > dy {
-                // Closer to vertical edge
-                (vec2(nx, 0.0), dx)
-            } else {
-                // Closer to horizontal edge
-                (vec2(0.0, ny), dy)
+        // Collide with entities
+        if entity.components.collidable.is_some() {
+            for other in self
+                .chunked_world
+                .entities_mut()
+                .filter(|other| other.id != entity_id && other.components.collidable.is_some())
+            {
+                Self::check_entity_collision(&mut entity, other);
             }
-        } else if dx <= 0.0 {
-            // Outside but closer to horizontal edge
-            (vec2(0.0, ny), dy)
-        } else if dy <= 0.0 {
-            // Outside but closer to vertical edge
-            (vec2(nx, 0.0), dx)
-        } else {
-            // Outside but closer to vertex
-            let normal = vec2(nx, ny);
-            let normal = normal / normal.len();
-            (normal, (dx * dx + dy * dy).sqrt())
-        };
+        }
 
-        if distance < circle_radius {
-            Some((normal, circle_radius - distance))
-        } else {
-            None
+        if let Some(size) = entity.size {
+            // Collide with tiles
+            for x in (-size.ceil() as i64)..(size.ceil() as i64 + 1) {
+                for y in (-size.ceil() as i64)..(size.ceil() as i64 + 1) {
+                    let tile_pos = get_tile_pos(vec2(x as f32, y as f32) + entity.pos.unwrap());
+                    self.check_entity_tile_collision(&mut entity, tile_pos);
+                }
+            }
+        }
+
+        *self.chunked_world.get_entity_mut(entity_id).unwrap() = entity;
+        self.chunked_world.update_entity(entity_id);
+    }
+
+    fn check_entity_collision(entity: &mut Entity, other: &mut Entity) {
+        match entities_collision(
+            entity.pos.unwrap(),
+            entity.size.unwrap(),
+            other.pos.unwrap(),
+            other.size.unwrap(),
+        ) {
+            CollisionResult::NoCollision => (),
+            CollisionResult::Collision {
+                penetration,
+                collision_normal,
+            } => match entity.collidable.as_ref().unwrap().collision_type {
+                CollisionType::Static => {
+                    *other.pos.as_mut().unwrap() += -collision_normal * penetration;
+                }
+                CollisionType::Pushable => {
+                    match other.collidable.as_ref().unwrap().collision_type {
+                        CollisionType::Static => {
+                            *entity.pos.as_mut().unwrap() += collision_normal * penetration;
+                        }
+                        CollisionType::Pushable => {
+                            *entity.pos.as_mut().unwrap() += collision_normal * penetration / 2.0;
+                            *other.pos.as_mut().unwrap() += collision_normal * penetration / 2.0;
+                        }
+                    }
+                }
+            },
         }
     }
+
+    fn check_entity_tile_collision(&mut self, entity: &mut Entity, tile_pos: Vec2<i64>) {
+        match self.chunked_world.get_tile(tile_pos) {
+            Some(tile) => {
+                if self.resource_pack.biome_properties[&tile.biome].collidable {
+                    match entity_tile_collision(
+                        entity.pos.unwrap(),
+                        entity.size.unwrap(),
+                        tile_pos,
+                        1.0,
+                    ) {
+                        CollisionResult::NoCollision => {}
+                        CollisionResult::Collision {
+                            penetration,
+                            collision_normal,
+                        } => {
+                            *entity.pos.as_mut().unwrap() += collision_normal * penetration;
+                        }
+                    }
+                }
+            }
+            None => (),
+        }
+    }
+
     fn player_action(&mut self, entity: &mut Entity) {
         if let Some(action) = entity.components.player.as_mut().unwrap().action.take() {
             match action {
